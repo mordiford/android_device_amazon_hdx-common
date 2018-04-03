583be8a355be9db4beb770a8794773be21354219
ggow <dontouchmyfood@gmail.com>
2018-04-03 09:59:23 +0100

    hidl: build renderscript HAL

--- hidl.mk
+++ hidl.mk
@@ -28,6 +28,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl
 
+# RenderScript
+PRODUCT_PACKAGES += \
+android.hardware.renderscript@1.0-impl
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl
